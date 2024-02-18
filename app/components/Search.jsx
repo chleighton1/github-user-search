3691f59c225c89f8042c02628617be6b010f735c
Charles Leighton <chleighton1@gmail.com>
2024-02-18 17:08:30 -0500

    Initial Commit

---
+++ app/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IconSearch from "@/public/icon-search";
+
+export default function Search(props) {
+  <div>
+    <form>
+      <h1>{props.title}</h1>
+      <IconSearch />
+      <input></input>
+      <button>Search</button>
+    </form>
+  </div>;
+}
